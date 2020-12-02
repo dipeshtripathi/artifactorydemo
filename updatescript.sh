16b272d99a1055e8e765f5f4be9bcd6754c64199
Dipesh Tripathi <dipesh.1708@hotmail.com>
2020-12-02 23:58:44 +0530

    a few more changes

--- updatescript.sh
+++ updatescript.sh
@@ -1,6 +1,6 @@
 git pull
 hash=$(git log --pretty=format:'%h' -n 1)
-git diff-tree --no-commit-id --name-only -r $hash > /Users/diptripa/git-updated-files/difference.txt
-cat /Users/diptripa/git-updated-files/difference.txt
+folderstr=$(git diff-tree --no-commit-id --name-only -r $hash) # > /Users/diptripa/git-updated-files/difference.txt
+#cat /Users/diptripa/git-updated-files/difference.txt
 
 
