61616a1f44439c3776f7628680cac8a2b238915e
Dipesh Tripathi <dipesh.1708@hotmail.com>
2020-12-02 21:31:32 +0530

    added the script to fetch changes in latest commit

---
+++ updatescript.sh
@@ -0,0 +1,5 @@
+git pull
+hash=$(git log --pretty=format:'%h' -n 1)
+git diff-tree --no-commit-id --name-only -r $hash > /Users/diptripa/git-updated-files/difference.txt
+cat /Users/diptripa/git-updated-files/difference.txt
+
