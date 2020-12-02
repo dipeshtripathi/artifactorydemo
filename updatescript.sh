365fd8252702c7330f5a469978226117be0306a3
Dipesh Tripathi <dipesh.1708@hotmail.com>
2020-12-03 00:29:34 +0530

    file dala re baba

--- updatescript.sh
+++ updatescript.sh
@@ -1,6 +1,9 @@
 git pull
 hash=$(git log --pretty=format:'%h' -n 1)
+echo $hash
 folderstr=$(git diff-tree --no-commit-id --name-only -r $hash) # > /Users/diptripa/git-updated-files/difference.txt
 #cat /Users/diptripa/git-updated-files/difference.txt
+echo $folderstr
 
+curl -u admin:admin -T $folderstr "http://artifactory.centralindia.cloudapp.azure.com/artifactory/artidemo/$folderstr"
 
